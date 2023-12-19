ad14f8666460df2e28309e71fe9714584c278b2f
LWRGitHub <LoganReynoldsLWR@gmail.com>
2023-12-19 10:46:37 -0800

    search res UI/UX

--- app.py
+++ app.py
@@ -16,32 +16,7 @@ app = Flask(__name__)
 def home():
     """Display the home page."""
 
-    context = {
-        "array": [
-            {
-                "src": "",
-                "alt": "img",
-                "href": "/",
-                "title": "example"
-            },
-            {
-                "src": "",
-                "alt": "img",
-                "href": "/art",
-                "title": "example2"
-            },
-        ],
-        "array2": [
-            {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/item/764524756"
-            },
-        ]
-    }
+    context = {}
 
     return render_template('pages/home.html', **context)
 
@@ -56,9 +31,42 @@ def page_not_found(e):
     return render_template('pages/404.html'), 404
 
 @app.route('/code_solution/<solution_id>')
-def art():
-    """Display the Code Solutions page."""
-    
+def code_solution(solution_id):
+    """Display the Code Solution page."""
+
+    solutions = [
+            {
+                "name": "3 Sum",
+                "site": "LeetCode",
+                "_id": "9827643rb27b27v",
+                "href": "/solution/9827643rb27b27v"
+            },
+            {
+                "name": "Two Sum",
+                "site": "LeetCode",
+                "_id": "liauwehfipuwefhlkw98237r23",
+                "href": "/solution/liauwehfipuwefhlkw98237r23"
+            },
+            {
+                "name": "Add Two Numbers",
+                "site": "LeetCode",
+                "_id": "lknas2387b24897b2",
+                "href": "/solution/lknas2387b24897b2"
+            }
+    ]
+
+    context = {}
+
+    for product in solutions:
+        if product["_id"] == solution_id:
+            context = product
+            break
+
+    return render_template('pages/code_solutions/code_solution.html', **context)
+
+@app.route('/search_solutions')
+def art_supplies():
+    """Display the search_solutions page."""
 
     context = {
         "page": {
@@ -73,71 +81,48 @@ def art():
         ],
         "products": [
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "3 Sum",
+                "site": "LeetCode",
+                "_id": "9827643rb27b27v",
+                "href": "/solution/9827643rb27b27v"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "Two Sum",
+                "site": "LeetCode",
+                "_id": "liauwehfipuwefhlkw98237r23",
+                "href": "/solution/liauwehfipuwefhlkw98237r23"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "Add Two Numbers",
+                "site": "LeetCode",
+                "_id": "lknas2387b24897b2",
+                "href": "/solution/lknas2387b24897b2"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "Add Two Promises",
+                "site": "LeetCode",
+                "_id": "injr237823b2497824",
+                "href": "/solution/injr237823b2497824"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "All Paths From Source to Target",
+                "site": "LeetCode",
+                "_id": "kj23498234b2498724",
+                "href": "/solution/kj23498234b2498724"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "Array Wrapper",
+                "site": "LeetCode",
+                "_id": "bkhj23893by234872",
+                "href": "/solution/bkhj23893by234872"
             },
             {
-                "src": "",
-                "alt": "img",
-                "title": "Example Product",
-                "_id": 764524756,
-                "info": "Ex",
-                "href": "/solution/764524756"
+                "name": "Binary Search Tree to Greater Sum Tree",
+                "site": "LeetCode",
+                "_id": "bh238713yuv2397213vyu2398h",
+                "href": "/solution/bh238713yuv2397213vyu2398h"
             }
         ]
-    }
-
-    return render_template('pages/code_solutions/code_solution.html', **context)
-
-@app.route('/search_solutions')
-def art_supplies():
-    """Display the search_solutions page."""
-
-    context = {
     }
 
     return render_template('pages/code_solutions/search_solutions.html', **context)
