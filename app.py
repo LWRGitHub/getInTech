2bcdd9e0a2bcb31e33b9035df3b106a547180807
LWRGitHub <LoganReynoldsLWR@gmail.com>
2024-05-19 12:10:28 -0700

    rm❌ Complex👾 JS in search🔍

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from web_forms import SearchForm
 from data.Solutions import Solutions
 from data.Solutions_info import Solutions_info
 from data.Swe_career_guide import Swe_career_guide 
+import pprint
+import math
 
 
 
@@ -229,30 +231,155 @@ def search():
 def search_solutions(query, page_number):
     """Display the search_solutions page."""
 
-    form = SearchForm()
+    page_number = int(page_number)
+    context = {}
+    context["page_number"] = page_number
 
-    context = {
-        "page": {
-            "name": "Code Solutions"
-        },
-        "other_example": [
-            {
-                "name": "Example",
-                "src": "",
-                "alt": "img"
-            },
-        ],
-        "form": form
-    }
+    def get_24_seach_res(res):
+        solution_to_send  = []
+        idx = page_number*24
+
+        # jump straight to the starting index based on page number use while loop
+        while idx < page_number*24+24 and idx < len(res):
+            # only send the necessary data
+            solution_to_send.append({
+                "name": res[idx]["name"],
+                "site": res[idx]["site"],
+                "_id": res[idx]["_id"],
+                "href": res[idx]["href"]
+            })
+            idx += 1
+        
+        return solution_to_send
 
     if query != "None":
         context["searched"] = query
-        context["solutions"] = [solution for solution in solutions if query.lower() in solution["name"].lower()]
+        # get all solutions that match the query
+        search_res = [solution for solution in solutions_info if query.lower() in solution["name"].lower()]
+
+        context["solutions"] = get_24_seach_res(search_res)
+        context["res_count"] = len(search_res)
+
         # context["solutions"] = [solution for solution in solutions if query.lower() in solution["name"].like('%{query}%')]
         # context["solutions"] = solutions.filter(solutions.name.like(f"%{query}%"))
     else:
-        context["searched"] = False
-        context["solutions"] = solutions   
+        context["searched"] = "None"
+        context["solutions"] = get_24_seach_res(solutions_info)
+        context["res_count"] = len(solutions_info)
+
+    # print('\n ********** \n')
+    # pprint.pprint(context["solutions"]) 
+    # print('\n ********** \n')
+
+    # Global variables to store pagination numbers
+    # pg_nav1_var = pg_nav2_var = pg_nav3_var = pg_nav4_var = None
+
+    def set_pagination_numbers(page_number, search_solutions_count):
+
+        # Get the current page number from the URL
+        pg_num = page_number + 1
+
+        # Determines how many pagination navigation numbers will show,
+        # depending on the current page and the number of search results
+        if pg_num > 3:
+            pg_nav4_var = math.ceil(search_solutions_count / 24)  # last page number
+
+            if pg_num != pg_nav4_var:
+                pg_nav1_var = pg_num - 2
+                pg_nav2_var = pg_num - 1
+                pg_nav3_var = pg_num
+            else:
+                pg_nav1_var = pg_num - 3
+                pg_nav2_var = pg_num - 2
+                pg_nav3_var = pg_num - 1
+
+        else:
+            if search_solutions_count > 95:
+                pg_nav1_var = 1
+                pg_nav2_var = 2
+                pg_nav3_var = 3
+                pg_nav4_var = math.ceil(search_solutions_count / 24)
+
+            elif search_solutions_count > 24:
+                pg_nav1_var = 1
+                pg_nav2_var = 2
+                pg_nav3_var = False
+                pg_nav4_var = math.ceil(search_solutions_count / 24)
+
+            else:
+                pg_nav1_var = 1
+                pg_nav2_var = False
+                pg_nav3_var = False
+                pg_nav4_var = False
+
+        return pg_nav1_var, pg_nav2_var, pg_nav3_var, pg_nav4_var
+    
+    # Set the pagination numbers
+    context["pg_nav1_var"], context["pg_nav2_var"], context["pg_nav3_var"], context["pg_nav4_var"] = set_pagination_numbers(page_number,context["res_count"])
+    pg_nav1_var = context["pg_nav1_var"]
+    pg_nav2_var = context["pg_nav2_var"]
+    pg_nav3_var = context["pg_nav3_var"]
+    pg_nav4_var = context["pg_nav4_var"]
+
+
+    # Define the underline style for page number workarounds for Safari & Firefox
+    pg_num_underline = 'text-decoration: underline solid #0280C8 !important; -webkit-text-decoration-line: underline !important;-webkit-text-decoration-color: #0280C8 !important; -webkit-text-decoration-style: solid !important;-webkit-text-decoration-thickness: 2px !important;' 
+
+    def set_page_link_style_1(page_number, pg_nav1_var, pg_nav2_var):
+
+        # Determine the border-radius style
+        if not pg_nav2_var:
+            border_radius = '5px 5px'
+        else:
+            border_radius = '0px 0px'
+
+        # Determine if underline style should be added
+        if pg_nav1_var == page_number+1:
+            underline_style = f'; {pg_num_underline}'
+        else:
+            underline_style = ';'
+
+        # Combine all styles
+        return f'border-radius: 5px {border_radius} 5px{underline_style}'
+
+    context["page_link_style_nav1"] = set_page_link_style_1(page_number, pg_nav1_var, pg_nav2_var)
+
+    def build_pagination_pg_num(pg_nav_var, next_pg_nav_var, pg_num, right_border_radius, pg_num_underline):
+        if pg_nav_var:
+            page_link_style = pg_num_underline if pg_nav_var == pg_num else ""
+            if not next_pg_nav_var:
+                page_link_style += right_border_radius
+            return {
+                'pg_nav_var': pg_nav_var,
+                'page_link_style': page_link_style
+            }
+        return None
+    
+    # Define the right border radius style
+    right_border_radius = "border-radius: 0px 5px 5px 0px;"
+
+    # Build pagination data
+    pagination_links = []
+    pagination_links.append(build_pagination_pg_num(pg_nav2_var, pg_nav3_var, page_number+1, right_border_radius, pg_num_underline))
+    pagination_links.append(build_pagination_pg_num(pg_nav3_var, pg_nav4_var, page_number+1, right_border_radius, pg_num_underline))
+    pagination_links = [link for link in pagination_links if link]
+
+    context["pagination_links"] = pagination_links
+
+    
+    
+    show_last_page_link = pg_nav4_var and pg_nav4_var != pg_nav2_var and pg_nav4_var != pg_nav3_var
+    last_page_link_style = None
+    # Determine the style for the last page link
+    if show_last_page_link:
+        if pg_nav4_var == page_number+1:
+            last_page_link_style = f"{right_border_radius} {pg_num_underline}"
+        else:
+            last_page_link_style = right_border_radius
+
+    context["show_last_page_link"] = show_last_page_link
+    context["last_page_link_style"] = last_page_link_style
+
 
     return render_template('pages/code_solutions/search_solutions.html', **context)
 
